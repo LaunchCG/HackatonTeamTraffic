6a5a675aede012ed8106db63c70df3af987bc52f
Hector Mendoza <hmendoza@launchcg.com>
2024-08-28 09:39:43 -0600

    Added collision detection.
    Added collision detection, but we realized that traffic lights are wrong, they let both intersecting lanes thru at the same time.

--- trafficSimulator.js
+++ trafficSimulator.js
@@ -12,7 +12,7 @@ class Car {
         this.lane = lane;
     }
 
-    update(trafficLights) {
+    update(trafficLights, cars) {
         // Check for traffic lights
         for (let light of trafficLights) {
             if (this.lane === 'horizontal' && light.y - 10 < this.y && this.y < light.y + 60 && light.state === 'red') {
@@ -25,8 +25,20 @@ class Car {
                 }
             }
         }
+
+        // Check for collisions
+        for (let otherCar of cars) {
+            if (otherCar !== this && this.isCollidingWith(otherCar)) {
+                return; // Stop the car if a collision is detected
+            }
+        }
+
         this.x += this.speed * Math.cos(this.direction);
         this.y += this.speed * Math.sin(this.direction);
+    }
+
+    isCollidingWith(otherCar) {
+        return !(this.x + 20 < otherCar.x || this.x > otherCar.x + 20 || this.y + 10 < otherCar.y || this.y > otherCar.y + 10);
     }
 
     draw() {
@@ -93,21 +105,18 @@ const roads = [
 
 // Create an array to hold cars
 const cars = [
-    // faster cars (original)
     new Car(100, 75, 2, 0, 'horizontal'), // Car on the first horizontal road
     new Car(200, 225, 2, 0, 'horizontal'), // Car on the second horizontal road
     new Car(300, 375, 2, 0, 'horizontal'), // Car on the third horizontal road
     new Car(175, 100, 2, Math.PI / 2, 'vertical'), // Car on the first vertical road
     new Car(375, 100, 2, Math.PI / 2, 'vertical'), // Car on the second vertical road
     new Car(575, 100, 2, Math.PI / 2, 'vertical'),  // Car on the third vertical road
-
-    // slower cars
-    new Car(100, 75, 1, 0, 'horizontal'), // Car on the first horizontal road
-    new Car(200, 225, 1, 0, 'horizontal'), // Car on the second horizontal road
-    new Car(300, 375, 1, 0, 'horizontal'), // Car on the third horizontal road
-    new Car(175, 100, 1, Math.PI / 2, 'vertical'), // Car on the first vertical road
-    new Car(375, 100, 1, Math.PI / 2, 'vertical'), // Car on the second vertical road
-    new Car(575, 100, 1, Math.PI / 2, 'vertical')  // Car on the third vertical road
+    new Car(400, 75, 2, 0, 'horizontal'), // Additional cars
+    new Car(500, 225, 2, 0, 'horizontal'),
+    new Car(600, 375, 2, 0, 'horizontal'),
+    new Car(175, 300, 2, Math.PI / 2, 'vertical'),
+    new Car(375, 300, 2, Math.PI / 2, 'vertical'),
+    new Car(575, 300, 2, Math.PI / 2, 'vertical')
 ];
 
 // Create an array to hold traffic lights
@@ -125,7 +134,7 @@ const trafficLights = [
 
 // Update car positions
 function updateCars() {
-    cars.forEach(car => car.update(trafficLights));
+    cars.forEach(car => car.update(trafficLights, cars));
 }
 
 // Update traffic lights
